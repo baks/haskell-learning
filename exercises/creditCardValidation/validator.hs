781e04d802b98a387741b348d02c6e02808f8481
Arkadiusz K <baks@users.noreply.github.com>
2015-12-13 22:13:01 +0100

    DoubleEveryOther function

--- exercises/creditCardValidation/validator.hs
+++ exercises/creditCardValidation/validator.hs
@@ -5,4 +5,16 @@ toDigits :: Integer -> [Integer]
 toDigits n
     | n < 0 = []
     | n == 0 = []
-    | otherwise = toDigits ((n- lastDigit n) `div` 10) ++ (lastDigit n : [])  
+    | otherwise = toDigits ((n- lastDigit n) `div` 10) ++ (lastDigit n : [])
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverse (toDigits n)
+
+doubleEveryOtherFromHead :: [Integer] -> [Integer]
+doubleEveryOtherFromHead [] = []
+doubleEveryOtherFromHead [x] = [x]
+doubleEveryOtherFromHead (x:(y:xs)) = [x,y*2] ++ doubleEveryOtherFromHead xs
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = reverse (doubleEveryOtherFromHead (reverse xs))
+
